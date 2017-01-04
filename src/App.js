c5899dca632353fda7a7b0bb2dd2282ba65660a3
Chu Hoang Son <chuson1996@gmail.com>
2017-01-04 11:27:09 +0200

    Slow down transition

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Motion, spring } from 'react-motion';
 import './App.css';
 
+const animate = (val) => spring(val, {
+  stiffness: 53,
+  damping: 27
+});
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -42,12 +47,12 @@ class App extends Component {
             perspective: `400px`
           }}>
           { this.state.boxes.map(({ top, left, width, height }, key) =>
-            <Motion key={key} defaultStyle={{ z: 0 }} style={{ z: spring(20) }}>
+            <Motion key={key} defaultStyle={{ z: 0 }} style={{ z: animate(20) }}>
               {({ z }) =>
                 <div className="segmenter__wrap" style={{
                   transform: `translateZ(${z}px)`
                 }}>
-                  <Motion defaultStyle={{ opacity: 0 }} style={{ opacity: spring(1) }}>
+                  <Motion defaultStyle={{ opacity: 0 }} style={{ opacity: animate(1) }}>
                     {({ opacity }) =>
                       <div className="segmenter__shadow" style={{
                         top: `${top}%`,
