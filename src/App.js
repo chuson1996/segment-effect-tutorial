afde78845355ce07dec9cb603bf0d3e7c9d6d09a
Chu Hoang Son <chuson1996@gmail.com>
2017-01-04 11:21:38 +0200

    add a bit of transition for the boxes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Motion, spring } from 'react-motion';
 import './App.css';
 
 class App extends Component {
@@ -10,19 +10,28 @@ class App extends Component {
         <div className="segmenter__pieces" style={{
             perspective: `400px`
           }}>
-          <div className="segmenter__wrap" style={{
-            transform: `translateZ(20px)`
-          }}>
-            <div className="segmenter__shadow" style={{
-              top: `12%`,
-              left: `70%`,
-              width: `20%`,
-              height: `10%`
-            }}></div>
-            <div className="segmenter__box" style={{
-              clipPath: `polygon(70% 12%, 90% 12%, 90% 22%, 70% 22%)`
-            }}></div>
-          </div>
+          <Motion defaultStyle={{ z: 0 }} style={{ z: spring(20) }}>
+            {({ z }) =>
+              <div className="segmenter__wrap" style={{
+                transform: `translateZ(${z}px)`
+              }}>
+                <Motion defaultStyle={{ opacity: 0 }} style={{ opacity: spring(1) }}>
+                  {({ opacity }) =>
+                    <div className="segmenter__shadow" style={{
+                      top: `12%`,
+                      left: `70%`,
+                      width: `20%`,
+                      height: `10%`,
+                      opacity
+                    }}></div>
+                  }
+                </Motion>
+                <div className="segmenter__box" style={{
+                  clipPath: `polygon(70% 12%, 90% 12%, 90% 22%, 70% 22%)`
+                }}></div>
+              </div>
+            }
+          </Motion>
         </div>
       </div>
     );
