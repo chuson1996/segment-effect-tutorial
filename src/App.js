931d35865b8cbfdc3e81fb971ae66d065a520c4d
Chu Hoang Son <chuson1996@gmail.com>
2017-01-04 11:10:31 +0200

    Fix a typo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="segmenter">
         <div className="segmenter__background"></div>
         <div className="segmenter__pieces">
-          <div className="segmenter_wrap">
+          <div className="segmenter__wrap">
             <div className="segmenter__box" style={{
               clipPath: `polygon(70% 12%, 90% 12%, 90% 22%, 70% 22%)`
             }}></div>
