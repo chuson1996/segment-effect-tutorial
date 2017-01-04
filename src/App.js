88a2fc23ed0939ddc6923ce2f7cae69ee453bfe7
Chu Hoang Son <chuson1996@gmail.com>
2017-01-04 11:07:08 +0200

    Add clipPath to boxes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div className="segmenter">
+        <div className="segmenter__background"></div>
+        <div className="segmenter__pieces">
+          <div className="segmenter_wrap">
+            <div className="segmenter__box" style={{
+              clipPath: `polygon(70% 12%, 90% 12%, 90% 22%, 70% 22%)`
+            }}></div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
